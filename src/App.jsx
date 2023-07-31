02c5f161f1c158d8664262bf2a0866e57f1aed3e
William Davidson <wj.davidson@outlook.com>
2023-07-31 12:01:23 +0200

    updated ps and scroll list

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { Suspense, useState } from 'react'
 import styles from './module/app.module.css'
 import ErrorBoundary from './ErrorBoundary'
 import TodoForm from './components/TodoForm'
+import NavTerminal from './components/navTerminal'
 
 export default function App() {
+  const [toggleTerminal, setToggleTerminal] = useState(false)
+
+  function handleToggleTerminal() {
+    setToggleTerminal(prevState => !prevState);
+  }
 
   return (
     <ErrorBoundary fallback={<div>Error...</div>}>
       <Suspense fallback={<div className={styles.loading}><img src='src/assets/Infinity-1.5s-200px.svg'/></div>}>
         <div className={styles.todoContainer}>
-          <TodoForm />
+          <TodoForm toggleTerminal={toggleTerminal}/>
+          <NavTerminal toggleTerminal={toggleTerminal} handleToggleTerminal={handleToggleTerminal}/>
         </div>
       </Suspense>
     </ErrorBoundary>
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import styles from '../module/todoform.module.css'
 
-export default function TodoForm() {
-    const [todoItem, setTodoItem] = useState('')
+function getAndUpdateInnerHeight() {
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+    
+    useEffect(() => {
+      const handleResize = () => {
+        setInnerHeight(window.innerHeight);
+      };
+  
+      window.addEventListener("resize", handleResize);
+  
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
+  
+    return innerHeight;
+  }
 
+export default function TodoForm(props) {
+    let yPosistion = 0;
+    const { toggleTerminal } = props
+    const elementRef = useRef(null);
+    const currentHeight = getAndUpdateInnerHeight();
+    const [todoItem, setTodoItem] = useState('');
     const todoArray = JSON.parse(localStorage.getItem('todoItems')) || [];
+    const todoItemsContainerHeight = {
+        height: toggleTerminal ? `${currentHeight - yPosistion - 750}px` : `${currentHeight - yPosistion - 300}px`
+    }
+
+    useEffect(() => {
+        if (elementRef.current) {
+            yPosistion = elementRef.current.getBoundingClientRect().top;
+        }
+    }, []);
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -23,7 +53,7 @@ export default function TodoForm() {
                 <input className={styles.todoInput} placeholder='// New Todo' value={todoItem} onChange={(e) => setTodoItem(e.target.value)}/>
                 <button className={styles.submitTodoBtn} type="submit"><FontAwesomeIcon icon={faPlus}/></button>
             </form>
-            <div className={styles.todoItemsContainer}>
+            <div className={styles.todoItemsContainer} style={todoItemsContainerHeight} ref={elementRef}>
                 <div>
                     <span className={styles.todoItemsUiPurple}>const</span>
                     <span className={styles.todoItemsUiYellow}> todoItems</span>
@@ -36,11 +66,11 @@ export default function TodoForm() {
                             <div className={styles.todoObjectContainer} key={todoIndex}>
                                 <span className={styles.todoItemsUiPurple}>{'{'}</span>
                                 <div className={styles.todoObjectContainer}>
-                                    <span className={styles.todoItemsUiRed}>todoItem: </span>
+                                    <span className={styles.todoItemsUiRed}>todoItem<span style={{color: '#abb2ba'}}>:</span> </span>
                                     <span className={styles.todoItemsUiGreen}>'{todo.todoItem}'<span style={{color: '#abb2ba'}}>,</span></span>
                                 </div>
                                 <div className={styles.todoObjectContainer}>
-                                    <span className={styles.todoItemsUiRed}>isDone: </span>
+                                    <span className={styles.todoItemsUiRed}>isDone<span style={{color: '#abb2ba'}}>:</span> </span>
                                     <span className={styles.todoItemsUiYellow}>{`${todo.isDone}`}</span>
                                 </div>
                                 <span className={styles.todoItemsUiPurple}>{'}'}{todoIndex < todoArray.length - 1 && <span style={{color: '#abb2ba'}}>,</span>}</span>
